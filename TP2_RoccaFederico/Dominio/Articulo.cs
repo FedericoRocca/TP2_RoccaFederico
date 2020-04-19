64f60d93622220b848e2c1dceeae77e6e8d65820
Federico 'Magus' Rocca <frocca17@gmail.com>
2020-04-19 02:17:12 -0300

    Se agregan clases "Articulo", "Marca" y "Categoria"

---
+++ TP2_RoccaFederico/Dominio/Articulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class Articulo
+    {
+        public string codigo { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public Marca marca { get; set; }
+        public Categoria categoria { get; set; }
+        public string imagen { get; set; }
+        public double precio { get; set; }
+
+        public Articulo()
+        {
+            marca = new Marca();
+            categoria = new Categoria();
+        }
+    }
+}
---
+++ TP2_RoccaFederico/Dominio/Marca.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class Marca
+    {
+        public string descripcion { get; set; }
+    }
+}
