795f13f21489f921b09d4452b97493f6e973a084
Federico 'Magus' Rocca <frocca17@gmail.com>
2020-05-27 22:28:16 -0300

    Se corrije y agrega obtención de Session en index.aspx

--- TP2_RoccaFederico/TP3_RoccaFederico/index.aspx.cs
+++ TP2_RoccaFederico/TP3_RoccaFederico/index.aspx.cs
@@ -10,72 +10,77 @@ namespace TP3_RoccaFederico
 {
     public partial class _Default : Page
     {
-		public List<Articulo> articulos = new List<Articulo>();
-		public List<Categoria> categorias = new List<Categoria>();
-		public CarritoArticulos miCarrito = new CarritoArticulos();
-		protected void Page_Load(object sender, EventArgs e)
+        public List<Articulo> articulos = new List<Articulo>();
+        public List<Categoria> categorias = new List<Categoria>();
+        public CarritoArticulos miCarrito = new CarritoArticulos();
+        protected void Page_Load(object sender, EventArgs e)
         {
-			try
-			{
-				CategoriaNegocio catNegocio = new CategoriaNegocio();
-				categorias = catNegocio.getCategorias();
+            try
+            {
+                CategoriaNegocio catNegocio = new CategoriaNegocio();
+                categorias = catNegocio.getCategorias();
 
-				ArticuloNegocio artNegocio = new ArticuloNegocio();
-				articulos = artNegocio.getArticulos();
+                ArticuloNegocio artNegocio = new ArticuloNegocio();
+                articulos = artNegocio.getArticulos();
 
-				if (!IsPostBack)
-				{
+                if (Session[Session.SessionID + "carrito"] != null)
+                {
+                    miCarrito = (CarritoArticulos)Session[Session.SessionID + "carrito"];
+                }
 
-					articulosRepeater.DataSource = articulos;
-					articulosRepeater.DataBind();
+                if (!IsPostBack)
+                {
 
-					repeaterCategorias.DataSource = categorias;
-					repeaterCategorias.DataBind();
-				}
-			}
-			catch (Exception ex)
-			{
-				Session.Add(Session.SessionID + "exception", ex);
-				Response.Redirect("error.aspx");
-			}
-		}
+                    articulosRepeater.DataSource = articulos;
+                    articulosRepeater.DataBind();
 
-		protected void btnAgregarAlCarrito_Click(object sender, EventArgs e)
-		{
-			try
-			{
-				if (Session[Session.SessionID + "carrito"] != null)
-				{
-					miCarrito = (CarritoArticulos)Session[Session.SessionID + "carrito"];
-				}
-				ArticuloNegocio artNegocio = new ArticuloNegocio();
-				int idArticulo = int.Parse(((Button)sender).CommandArgument);
-				miCarrito.elementos.Add( artNegocio.getArticuloByID(idArticulo) );
-				Session.Add( Session.SessionID + "carrito", miCarrito );
-				
-			}
-			catch (Exception ex)
-			{
-				Session.Add(Session.SessionID + "exception", ex);
-				Response.Redirect("error.aspx");
-			}
-		}
+                    repeaterCategorias.DataSource = categorias;
+                    repeaterCategorias.DataBind();
+                }
+            }
+            catch (Exception ex)
+            {
+                Session.Add(Session.SessionID + "exception", ex);
+                Response.Redirect("error.aspx");
+            }
+        }
 
-		protected void btnFiltrarCategoria_Click(object sender, EventArgs e)
-		{
-			try
-			{
-				int idCategoria = int.Parse(((Button)sender).CommandArgument);
-				articulos = articulos.Where(x => x.categoria.id == idCategoria).ToList();
-				articulosRepeater.DataSource = articulos;
-				articulosRepeater.DataBind();
-			}
-			catch (Exception ex)
-			{
-				Session.Add(Session.SessionID + "exception", ex);
-				Response.Redirect("error.aspx");
-			}
-		}
+        protected void btnAgregarAlCarrito_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Session[Session.SessionID + "carrito"] != null)
+                {
+                    miCarrito = (CarritoArticulos)Session[Session.SessionID + "carrito"];
+                }
+                ArticuloNegocio artNegocio = new ArticuloNegocio();
+                int idArticulo = int.Parse(((Button)sender).CommandArgument);
+                miCarrito.elementos.Add(artNegocio.getArticuloByID(idArticulo));
+                Session.Add(Session.SessionID + "carrito", miCarrito);
 
-	}
+            }
+            catch (Exception ex)
+            {
+                Session.Add(Session.SessionID + "exception", ex);
+                Response.Redirect("error.aspx");
+            }
+        }
+
+        protected void btnFiltrarCategoria_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int idCategoria = int.Parse(((Button)sender).CommandArgument);
+                articulos = articulos.Where(x => x.categoria.id == idCategoria).ToList();
+                articulosRepeater.DataSource = articulos;
+                articulosRepeater.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Session.Add(Session.SessionID + "exception", ex);
+                Response.Redirect("error.aspx");
+            }
+        }
+
+    }
 }
